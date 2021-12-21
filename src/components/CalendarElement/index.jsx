f25dc715c0083033091ff57435a4fb3faf22604e
manoyuuki <bareeboulu@yahoo.co.jp>
2021-12-21 17:04:04 +0900

    first commit

---
+++ src/components/CalendarElement/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import * as styles from './style.css'
+import { Typography } from '@material-ui/core'
+import dayjs from 'dayjs'
+import { isSameMonth, isSameDay, isFirstDay, getMonth } from '../../services/calendar'
+import Schedule from '../Schedule'
+
+const CalendarElement = ({
+  day,
+  month,
+  schedules,
+  ...props
+}) => {
+  const currentMonth = getMonth(month)
+  const isCurrentMonth = isSameMonth(day, currentMonth)
+  const textColor = isCurrentMonth ? "textPrimary" : "textSecondary"
+  const format = isFirstDay(day) ? "M月D日" : "D"
+  const today = dayjs()
+  const isToday = isSameDay(day, today)
+
+  return (
+    <div className={styles.element}>
+      <Typography
+        className={styles.date}
+        color={textColor}
+        align="center"
+        variant="caption"
+        component="div"
+      >
+        <span className={isToday ? styles.today : ""}>
+          {day.format(format)}
+        </span>
+      </Typography>
+      <div className={styles.schedules}>
+        {schedules.map(e => (
+          <Schedule key={e.id} schedule={e} {...props} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default CalendarElement
